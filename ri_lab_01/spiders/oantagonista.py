73b03480fcf8e6f67f5ee8407b87607944ee9ce4
Joao <joao.soares@ccc.ufcg.edu.br>
2019-04-05 21:52:24 -0300

    metodos

--- ri_lab_01/spiders/oantagonista.py
+++ ri_lab_01/spiders/oantagonista.py
@@ -8,24 +8,34 @@ from ri_lab_01.items import RiLab01CommentItem
 
 class OantagonistaSpider(scrapy.Spider):
     name = 'oantagonista'
-    allowed_domains = ['oantagonista.com']
-    start_urls = []
-
-    def __init__(self, *a, **kw):
-        super(OantagonistaSpider, self).__init__(*a, **kw)
-        with open('seeds/oantagonista.json') as json_file:
-                data = json.load(json_file)
-        self.start_urls = list(data.values())
+    start_urls = ['https://www.oantagonista.com/pagina/15/']
 
     def parse(self, response):
-        #
-        # inclua seu código aqui
-        #
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
-        #
-        #
-        #
+        links = response.xpath('//ul//li//a[re:test(@href, "https://www.oantagonista.com/assuntos/")]/@href').getall()
+        for link in links:
+            yield scrapy.Request(
+            response.urljoin(link),
+            callback = self.parse_category
+            )
+    def parse_category(self,response):
+
+        news = response.css('a.article_link::attr(href)').getall()
+        for new in news:
+            yield scrapy.Request(
+            response.urljoin(new),
+            callback=self.parse_new
+            )
+    def parse_new(self, response):
+        title = response.css('article h1::text').get()
+        date = response.css('article time::text').get()
+        text = response.css('article p::text').get()
+        author = response.css('article.comment-body footer div b.fn::text').get()
+        category = response.css('span a::text').get()
+        yield{
+        'title' : title,
+        'date' : date,
+        'text' : text,
+        'auhor' : author,
+        'category' : category,
+        'url' : response.url
+        }
